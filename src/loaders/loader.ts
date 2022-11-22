90f333c89f31debecb2db05b7fbf51578d926bf6
SGF <SGF@sourcegoodfood.com>
2022-11-22 05:54:59 +0000

    firstfix:  version

--- src/loaders/loader.ts
+++ src/loaders/loader.ts
@@ -2,4 +2,10 @@ import { AwilixContainer } from 'awilix';
 
 export default (container: AwilixContainer, config: Record<string, unknown>): void | Promise<void> => {
   /* Implement your own loader. */
+  try {
+    const shopifyService = container.resolve("shopifyService")
+    //  await shopifyService.importShopify()
+  } catch (err) {
+    console.log(err)
+  }
 }
