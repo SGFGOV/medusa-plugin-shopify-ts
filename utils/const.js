43b6d01ab30fe36d3410b16c463e582a90bfd292
SGF <SGF@sourcegoodfood.com>
2022-12-06 10:17:11 +0000

    chore: clear residue

--- utils/const.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IGNORE_THRESHOLD = exports.INCLUDE_PRESENTMENT_PRICES = void 0;
-exports.INCLUDE_PRESENTMENT_PRICES = {
-    "X-Shopify-Api-Features": "include-presentment-prices",
-};
-exports.IGNORE_THRESHOLD = 2;
-//# sourceMappingURL=const.js.map
--- utils/parse-price.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parsePrice = void 0;
-function parsePrice(price) {
-    return parseInt(Number(price).toFixed(2)) * 100;
-}
-exports.parsePrice = parsePrice;
-//# sourceMappingURL=parse-price.js.map
